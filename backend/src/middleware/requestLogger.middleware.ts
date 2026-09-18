f52a5d5b66fefb79870a32ed8ce7106cff59ac7a
krupal-036 <krupalfataniya007@gmail.com>
2026-09-18 21:19:23 +0530

    feat: refactor requestLogger middleware to include request ID in logs

--- backend/src/middleware/requestLogger.middleware.ts
+++ backend/src/middleware/requestLogger.middleware.ts
@@ -2,8 +2,12 @@
 import { Request, Response, NextFunction } from "express";
 import { AppLogger } from "../utils/handlers/logHandler";
 
-export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
-    const timestamp = new Date().toISOString();
-    AppLogger.log(`[${timestamp}] ${req.method} ${req.originalUrl} - IP: ${req.ip}`);
-    next();
-};
+export const requestLogger = () => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const timestamp = new Date().toISOString();
+        AppLogger.log(
+            `[${timestamp}] ${req.method} ${req.originalUrl} - IP: ${req.ip} REQ ID ${req.headers["x-request-id"]}`,
+        );
+        next();
+    }
+}
