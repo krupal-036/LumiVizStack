07d2a68694dd601fba64a29d8e6afb30d1480b5c
krupal-036 <krupalfataniya007@gmail.com>
2026-09-18 21:18:44 +0530

    refactor: rename startDevServer to startServer and enhance shutdown handling

--- backend/src/utils/handlers/serverHandler.ts
+++ backend/src/utils/handlers/serverHandler.ts
@@ -2,11 +2,23 @@
 import { AppConfig } from "../../config/app.config";
 import { AppLogger } from "./logHandler";
 
-export const startDevServer = (app: any): void => {
+export const startServer = (app: any): void => {
     if (AppConfig.NODE_ENV !== "production") {
         const PORT = AppConfig.PORT;
-        app.listen(PORT, "0.0.0.0", () => {
-            AppLogger.log(`[${AppConfig.NODE_ENV}] Server running on http://localhost:${PORT}`);
+        const server = app.listen(PORT, "0.0.0.0", () => {
+            AppLogger.log(
+                `[${AppConfig.NODE_ENV}] Server running locally on http://localhost:${PORT}`,
+            );
         });
+        const shutdown = (signal: string) => {
+            AppLogger.log(`Received ${signal}. Closing HTTP server...`);
+            server.close(() => {
+                AppLogger.log("HTTP server closed.");
+                process.exit(0);
+            });
+        };
+
+        process.on("SIGTERM", () => shutdown("SIGTERM"));
+        process.on("SIGINT", () => shutdown("SIGINT"));
     }
 };
