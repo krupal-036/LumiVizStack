3e2cef7721604fe0e407112b044ea50ab4bff4ce
krupal-036 <krupalfataniya007@gmail.com>
2026-09-18 21:35:25 +0530

    feat: add preloadStyles plugin to enhance CSS loading performance

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,18 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import preloadStyles from "./plugins/preloadStyles.js"
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     base: '/',
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(),
+      tailwindcss(),
+      preloadStyles(),
+    ],
     server: {
       proxy: (mode === 'development' || env.VITE_ENABLE_PROXY === 'true') ? {
         '/api': {
@@ -19,6 +24,7 @@ export default defineConfig(({ mode }) => {
       } : undefined,
     },
     build: {
+      cssCodeSplit: true,
       chunkSizeWarningLimit: 1000,
       rollupOptions: {
         output: {
